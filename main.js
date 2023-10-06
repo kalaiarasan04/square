157ac1f17a3641b968ea1455bf51b0fd73330a0c
kalaiarasan04 <kalaiarasan0t@gmail.com>
2023-10-06 21:48:58 +0530

    update

---
+++ main.js
@@ -0,0 +1,13 @@
+
+// square method in arrow function
+
+let user=parseInt(prompt("enter a number"));
+
+let ans=function(user){
+	
+    return user*user
+	
+}
+let res=ans(user);
+
+document.write(res);
